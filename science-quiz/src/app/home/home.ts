304dab65c11eb0d2e8dcad3d72ee09f3d3f3df0e
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-30 00:05:48 -0500

    añado el home.ts, la ruta y el home.html

--- science-quiz/src/app/home/home.ts
+++ science-quiz/src/app/home/home.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { QuizService } from '../quiz.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css'
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class Home {
-
+export class HomeComponent {
+  constructor(private quizService: QuizService) {
+    // Reiniciamos el quiz cada vez que volvemos a la página de inicio
+    this.quizService.resetQuiz();
+  }
 }
